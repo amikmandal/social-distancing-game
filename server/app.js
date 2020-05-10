31c987923d3e9d9a42b3fd4c1cddcd9b07870385
dvt5 <dvt5@duke.edu>
2020-05-11 00:39:31 +0700

    add server logic

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const Express = require("express")();
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+Http.listen(3000, () => {
+  console.log("Listening at :3000...");
+});
+
+var position = {
+  x: 200,
+  y: 200
+};
+
+Socketio.on("connection", socket => {
+  socket.emit("position", position);
+});
+
