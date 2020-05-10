ab916e1c211c251007877b172b9271f2aae03707
Amik Mandal <amik.mandal@duke.edu>
2020-05-11 04:04:45 +0530

    tied to mouse

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -4,11 +4,12 @@ class Canvas extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {x: 640, y: 320};
+        this.state = {x: 0, y: 0};
+        this.state = {w: window.innerWidth, h: window.innerHeight};
     }
 
     componentDidMount() {
-       this.drawPlayer(640,320);
+       this.drawPlayer(this.state.x,this.state.y);
     }
 
     _onMouseMove(e) {
@@ -17,24 +18,16 @@ class Canvas extends React.Component {
     }
 
     drawPlayer(a,b){
-        this.context.clearRect(this.state.x - 11, this.state.y - 11, this.state.x + 11, this.state.y + 11);
-        // this.context.beginPath();
-        // this.context.arc(this.state.x,this.state.y,10,0,2*Math.PI);
-        // this.context.fillStyle = '#ffffff'
-        // this.context.fill();
-        // this.context.strokeStyle = '#ffffff'
-        // this.context.stroke();
-        // this.context.closePath();
+        this.context.clearRect(0, 0, this.state.w, this.state.h);
         this.context.beginPath();
         this.context.arc(a,b,10,0,2*Math.PI)
         this.context.fill();
-        //this.setState({x: a,y: b});
     }
 
     render() {
         return (
             <div onMouseMove={this._onMouseMove.bind(this)} style={{cursor: "none"}}>
-            <canvas ref={(c) => this.context = c.getContext('2d')} width={1280} height={720} style={{border: "1px solid black",}}></canvas>
+            <canvas ref={(c) => this.context = c.getContext('2d')} width={this.state.w} height={this.state.h} style={{border: "1px solid black",}}></canvas>
             </div>
         );
     }
