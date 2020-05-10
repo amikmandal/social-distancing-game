d2cb7a96f700a147954902459210e8acab3051b2
Amik Mandal <amik.mandal@duke.edu>
2020-05-11 01:51:59 +0530

    mouse pointers

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 class Canvas extends React.Component {
 
+    componentDidMount() {
+        this.context.beginPath();
+        this.context.arc(640,320,10,0,2*Math.PI)
+        this.context.fill();
+    }
+
+    _onMouseMove(e) {
+        console.log(e.screenX, e.screenY)
+    }
+
     render() {
         return (
-            <div>
-            <canvas ref={(c) => this.context = c.getContext('2d')} width={1380} height={720} style={{border: "1px solid black",}}></canvas>
+            <div onMouseMove={this._onMouseMove.bind(this)}>
+            <canvas ref={(c) => this.context = c.getContext('2d')} width={1280} height={720} style={{border: "1px solid black",}}></canvas>
             </div>
         );
     }
