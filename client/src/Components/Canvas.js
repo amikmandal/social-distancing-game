40fbb765b5c12f7a007908703d338c85ecd3e293
Amik Mandal <amik.mandal@duke.edu>
2020-05-12 04:03:06 +0530

    added borders, changed back to state

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -4,34 +4,58 @@ class Canvas extends React.Component {
 
     constructor(props) {
         super(props);
+
         this.width = window.innerWidth
         this.height = window.innerHeight
-        this.mouseX = this.width/2
-        this.mouseY = this.height/2
-        this.x = this.width/2
-        this.y = this.height/2
-        //this.state = {x: this.width/2, y: this.height/2};
+        this.radius = 20
+        this.frameTime = 16
+        this.weight = this.frameTime/1000;
+
+        this.state = {mouseX: this.width/2, mouseY: this.height/2, x: this.width/2, y: this.height/2}
+
         this._onMouseMove = this._onMouseMove.bind(this)
         this.setContext = this.setContext.bind(this);
+        this.updateCanvas = this.updateCanvas.bind(this);
     }
 
     componentDidMount() {
-       this.interval = setInterval(() => {
-            const diffX = this.mouseX - this.x
-            const diffY = this.mouseY - this.y
-            if((diffX * diffX + diffY * diffY > 400)){ //radius squared
-                this.x += diffX*0.016
-                this.y += diffY*0.016
+       this.interval = setInterval(this.updateCanvas,this.frameTime);
+       this.drawPlayer()
+    }
+
+    updateCanvas(){
+        const diffX = this.state.mouseX - this.state.x
+        const diffY = this.state.mouseY - this.state.y
+        if((diffX * diffX + diffY * diffY > this.radius * this.radius)){
+            var newX = this.state.x + diffX * this.weight
+            var newY = this.state.y + diffY * this.weight
+            if(!this.isInBoundsX(newX)){
+                newX = this.state.x
             }
-            this.drawPlayer()  
-        },16);
+            if(!this.isInBoundsY(newY)){
+                newY = this.state.y
+            }
+            this.setState({x: newX, y: newY})
+            
+            this.drawPlayer()
+        }
+        
+    }
+
+    isInBoundsX(x){
+        if(x<this.radius || x>2*(this.width - this.radius))
+            return false;
+        return true;
+    }
+
+    isInBoundsY(y){
+        if(y<this.radius || y>2*(this.width - this.radius))
+            return false;
+        return true;
     }
 
     _onMouseMove(e) {
-        this.mouseX = e.screenX
-        this.mouseY = e.screenY - 75
-        //this.setState({mouseX: e.screenX, mouseY: e.screenY - 75})
-        //console.log(e.screenX,e.screenY-75);
+        this.setState({mouseX: e.screenX, mouseY: e.screenY - 75})
     }
 
     setContext(c) {
@@ -41,7 +65,7 @@ class Canvas extends React.Component {
     drawPlayer(){
         this.canvas.clearRect(0, 0, this.width, this.height);
         this.canvas.beginPath();
-        this.canvas.arc(this.x,this.y,20,0,2*Math.PI)
+        this.canvas.arc(this.state.x,this.state.y,this.radius,0,2*Math.PI)
         this.canvas.fill();
     }
 
