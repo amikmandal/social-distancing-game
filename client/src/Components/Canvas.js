e01dbe9dc76b3f52f6be821465f83f1c9873daf3
Amik Mandal <amik.mandal@duke.edu>
2020-05-12 01:21:48 +0530

    made blob stationary

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -1,40 +1,46 @@
 import React from 'react';
-import io from "socket.io-client";
 
 class Canvas extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {x: 0, y: 0};
         this.state = {w: window.innerWidth, h: window.innerHeight};
-        this.socket = io("http://localhost:3000");
+        this._onMouseMove = this._onMouseMove.bind(this);
+        this.setContext = this.setContext.bind(this);
+        //this.state = {time: Date.now()}
     }
 
     componentDidMount() {
-       this.drawPlayer(this.state.x,this.state.y);
+       this.interval = setInterval(this.updateBoard(),16);
+       this.drawPlayer(this.state.w/2,this.state.h/2);
+       //console.log(this.state.w/2,this.state.h/2);
+    }
+
+    updateBoard(){
+        ;
     }
 
     _onMouseMove(e) {
-        var dir = {x: e.screenX, y: e.screenY - 75};
-        this.socket.emit("move", dir);
         //this.drawPlayer(e.screenX, e.screenY-75);
-        this.socket.on("position", data => {
-            console.log(data.x, data.y);
-            this.drawPlayer(data.x, data.y)
-        })
+        
+        console.log(e.screenX,e.screenY-75);
+    }
+
+    setContext(c) {
+        this.context = c.getContext('2d');
     }
 
     drawPlayer(a,b){
         this.context.clearRect(0, 0, this.state.w, this.state.h);
         this.context.beginPath();
-        this.context.arc(a,b,10,0,2*Math.PI);
+        this.context.arc(a,b,20,0,2*Math.PI)
         this.context.fill();
     }
 
     render() {
         return (
-            <div onMouseMove={this._onMouseMove.bind(this)} style={{cursor: "none"}}>
-            <canvas ref={(c) => this.context = c.getContext('2d')} width={this.state.w} height={this.state.h} style={{border: "1px solid black",}}/>
+            <div onMouseMove={this._onMouseMove}>
+            <canvas ref={this.setContext} width={this.state.w} height={this.state.h} style={{border: "1px solid black"}}></canvas>
             </div>
         );
     }
