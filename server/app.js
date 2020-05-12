f314385d9e50693ceeb1b9e2166b9efc50c12a24
Amik Mandal <amik.mandal@duke.edu>
2020-05-13 00:01:42 +0530

    issue with duplicate server

--- server/app.js
+++ server/app.js
@@ -1,31 +1,85 @@
-const Express = require("express")();
-const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
+const express = require("express");
+const http = require("http")
+const socketIo = require("socket.io")
 
-var positions = [ {x: 640, y: 320}, {x: 120, y: 240} ]
-var availablePlayer = 0
+const app = express()
+app.get('/', (req,res) => {
+  res.send("Server is running ... ");
+})
 
-var data = {
-  players: {
-    /*
-      id: 124325423
-      x: 324325
-      y: 1234325325
-      mouseX: 124325
-      mouseY: 325325
-    */
-  }
-}
+const server = http.Server(app);
+server.listen(3000);
 
-Http.listen(3000, () => {
-  console.log("Listening at :3000...");
-});
+const io = socketIo(server);
 
-Socketio.on('connection', data => {
-  console.log('hello')
-  console.log(data.id)
-  Socketio.emit('position', positions[availablePlayer]);
-});
+// var positions = [ {x: 640, y: 320}, {x: 120, y: 240} ]
+// var availablePlayer = 0
+
+var players = {};
+
+// players = {
+//   '3124325235235235' : {
+//     x: 640,
+//     y: 320,
+//     mouseX: 100,
+//     mouseY: 200
+//   },
+//   '2423532643643643' : {
+//     x: 640,
+//     y: 200,
+//     mouseX: 900,
+//     mouseY: 500
+//   }
+// }
+
+//console.log('hi');
+
+
+io.on('connection', socket => {
+
+  console.log('client connected');
+  const id = Date.now().toString()
+  players[id] = {}
+  //players[id] = {s: socket};
+  socket.emit('id', id);
+
+  socket.on('info', data => {
+    players[id].w = data.w
+    players[id].h = data.h
+    players[id].x = 640
+    players[id].y = 320
+    io.emit('position', {x: players[id].x, y: players[id].y} )
+  })
+
+  socket.on('mouse', data => {
+    players[id].mouseX = data.mouseX
+    players[id].mouseY = data.mouseY
+  })
+
+  setInterval(() => {
+    players[id].x += 5
+    players[id].y += 5
+    io.emit('position', {x: players[id].x, y: players[id].y} )
+  },1000);
+
+  socket.on('disconnect', () => {
+    console.log('disconnected ', players[id])
+    players = {}
+    //delete players.id
+  });
+
+})
+
+// Socketio.on('connection', socket => {
+//   socket.emit('init','Hello Client!')
+// })
+
+// Socketio.on('info', data => {
+//   console.log('hello')
+//   console.log(data.id)
+//   Socketio.emit('position', positions[availablePlayer]);
+//   availablePlayer++;
+// });
 
 // Socketio.on("connection", socket => {
 //   socket.emit("position", position);
