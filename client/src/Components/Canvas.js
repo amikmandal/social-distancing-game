d46cb3a446b502c7c09b91d9326893f9b4b5be6e
Amik Mandal <amik.mandal@duke.edu>
2020-05-12 20:13:09 +0530

    set constant speed

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -8,8 +8,8 @@ class Canvas extends React.Component {
         this.width = window.innerWidth
         this.height = window.innerHeight
         this.radius = 20
-        this.frameTime = 16
-        this.weight = this.frameTime/1000;
+        this.frameTime = 32
+        this.speed = 10
 
         this.state = {mouseX: this.width/2, mouseY: this.height/2, x: this.width/2, y: this.height/2}
 
@@ -26,32 +26,45 @@ class Canvas extends React.Component {
     updateCanvas(){
         const diffX = this.state.mouseX - this.state.x
         const diffY = this.state.mouseY - this.state.y
-        if((diffX * diffX + diffY * diffY > this.radius * this.radius)){
-            var newX = this.state.x + diffX * this.weight
-            var newY = this.state.y + diffY * this.weight
-            if(!this.isInBoundsX(newX)){
-                newX = this.state.x
-            }
-            if(!this.isInBoundsY(newY)){
-                newY = this.state.y
-            }
-            this.setState({x: newX, y: newY})
-            
-            this.drawPlayer()
+        const angle = Math.atan(diffY/diffX)
+        const factor = diffX > 0 ? 1 : -1;
+        var newX = 0.0
+        var newY = 0.0
+        if(this.distance(diffX,diffY) > this.radius){
+            //constant speed
+            newX = this.state.x + factor * this.speed*Math.cos(angle)
+            newY = this.state.y + factor * this.speed*Math.sin(angle)
+
+            //exponential speed
+            // newX = this.state.x + 0.016 * diffX
+            // newY = this.state.y + 0.016 * diffY
         }
+        // } else {
+        //     newX = this.state.x + diffX*0.016
+        //     newY = this.state.y + diffY*0.016
+        // }
+        if(this.outOfBoundsX(newX)){
+            newX = this.state.x
+        }
+        if(this.OutOfBoundsY(newY)){
+            newY = this.state.y
+        }
+        this.setState({x: newX, y: newY})
+        
+        this.drawPlayer()
         
     }
 
-    isInBoundsX(x){
-        if(x<this.radius || x>2*(this.width - this.radius))
-            return false;
-        return true;
+    distance(a,b){
+        return Math.sqrt(a*a+b*b)
     }
 
-    isInBoundsY(y){
-        if(y<this.radius || y>2*(this.width - this.radius))
-            return false;
-        return true;
+    outOfBoundsX(x){
+        return x<this.radius || x>2*(this.width - this.radius)
+    }
+
+    outOfBoundsY(y){
+        return y<this.radius || y>2*(this.width - this.radius)
     }
 
     _onMouseMove(e) {
