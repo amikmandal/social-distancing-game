f55b3c6679d23db8bbbb0ca44a164cbf6c25986b
Amik Mandal <amik.mandal@duke.edu>
2020-05-12 21:17:55 +0530

    save progress

--- server/app.js
+++ server/app.js
@@ -2,6 +2,21 @@ const Express = require("express")();
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http);
 
+var positions = [ {x: 640, y: 320}, {x: 120, y: 240} ]
+var availablePlayer = 0
+
+var data = {
+  players: {
+    /*
+      id: 124325423
+      x: 324325
+      y: 1234325325
+      mouseX: 124325
+      mouseY: 325325
+    */
+  }
+}
+
 Http.listen(3000, () => {
   console.log("Listening at :3000...");
 });
@@ -11,16 +26,17 @@ var position = {
   y: 200
 };
 
-Socketio.on("connection", socket => {
-  socket.emit("position", position);
+Socketio.on('connection', socket => {
+  console.log(position[availablePlayer]);
+  Socketio.emit('position', position[availablePlayer]);
 });
 
-Socketio.on("connection", socket => {
-  socket.emit("position", position);
-  socket.on("move", data => {
-    switch(data) {
+// Socketio.on("connection", socket => {
+//   socket.emit("position", position);
+//   socket.on("move", data => {
+//     switch(data) {
 
-    }
-  });
-});
+//     }
+//   });
+// });
 
