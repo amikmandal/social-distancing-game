87164f88d9f90d6fc3a904d24fc16a0ed5751b05
Amik Mandal <amik.mandal@duke.edu>
2020-05-19 16:44:53 +0530

    added background image

--- client/src/Components/Canvas.js
+++ client/src/Components/Canvas.js
@@ -1,10 +1,14 @@
 import React from 'react';
+//import background from '../assets/images/bg.png'
 
 class Canvas extends React.Component {
 
     constructor(props) {
         super(props);
 
+        //this.bg = 'https://www.xmple.com/wallpaper/graph-paper-white-blue-grid-1920x1080-c2-ffffff-add8e6-l2-7-28-a-0-f-20.svg';
+        this.bg = 'https://lh3.googleusercontent.com/proxy/cCm7L-tScWQ-EpApONYmNtNQRrHAyMQY3f4bybiQV4Tr8JZkgc4R92fSZrRF4c7V-zOgziaZ_q-uAsoXUQ8hAm-gsb8WIrnr3uN2Tiod9MpoAi8LDVolzA'
+        
         this.id = this.props.id
         this.width = window.innerWidth
         this.height = window.innerHeight
@@ -12,17 +16,43 @@ class Canvas extends React.Component {
         this.frameTime = 32
         this.speed = 10
 
-        //this.state = {mouseX: this.width/2, mouseY: this.height/2, x: this.width/2, y: this.height/2}
-
+        this.setContextBgCanvas = this.setContextBgCanvas.bind(this);
         this.setContext = this.setContext.bind(this);
     }
 
     componentDidMount() {
         this.props.onRef(this)
+
+        // const img = new Image();
+        // img.src = this.bg;
+
+        // img.onload = function(){
+        //     var ptrn = this.bgCanvas.createPattern(img, 'repeat');
+        //     this.bgCanvas.fillStyle = ptrn;
+        //     this.bgCanvas.fillRect(0, 0, this.width, this.height);
+        // }
     }
 
     componentWillUnmount() {
         this.props.onRef(undefined)
+    }
+
+    setContextBgCanvas(c) {
+        this.bgCanvas = c.getContext('2d');
+
+        const img = new Image();
+        img.src = this.bg;
+        img.onload = this.drawBg (this.bgCanvas,img,this.width,this.height)
+    }
+
+    drawBg(bgCanvas,img,width,height) {
+        //console.log(img)
+        //bgCanvas.drawImage(img,0,0);
+        var ptrn = bgCanvas.createPattern(img, 'repeat');
+        bgCanvas.rect(0,0,width,height);
+        bgCanvas.fillStyle = ptrn;
+        bgCanvas.fill();
+        //bgCanvas.fillRect(0, 0, width, height);
     }
 
     setContext(c) {
@@ -49,7 +79,8 @@ class Canvas extends React.Component {
     render() {
         return (
           <div>
-              <canvas ref={this.setContext} width={this.width} height={this.height} style={{border: "1px solid black"}}/>
+              <canvas ref={this.setContextBgCanvas} width={this.width} height={this.height} style={{position: "absolute"}}/>
+              <canvas ref={this.setContext} width={this.width} height={this.height} style={{position: "absolute", border: "1px solid black"}}/>
           </div>
         );
     }
